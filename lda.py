41c5527262153d4cebeda1d7b47c4656ef44b317
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2021-06-27 12:08:51 +0900

    add coherence

--- lda.py
+++ lda.py
@@ -6,8 +6,10 @@ from nltk.stem.porter import PorterStemmer
 from gensim.models.coherencemodel import CoherenceModel
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from factor_analyzer import FactorAnalyzer, Rotator
 import pprint
+import pickle
 
 
 def create_gensim_lda_model(dictionary, corpus, number_of_topics, words):
@@ -25,8 +27,8 @@ def compute_coherence_values(
         corpus,
         documents,
         stop,
-        start=2,
-        step=3):
+        start=1,
+        step=1):
     coherence_values = []
     model_list = []
     for num_topics in range(start, stop, step):
@@ -44,20 +46,60 @@ def compute_coherence_values(
     return model_list, coherence_values
 
 
+def compute_perplexity_values(
+        dictionary,
+        corpus,
+        documents,
+        stop,
+        start=1,
+        step=1):
+    perplexity_values = []
+    model_list = []
+    for num_topics in range(start, stop, step):
+        model = LdaModel(
+            corpus,
+            num_topics=num_topics,
+            id2word=dictionary)
+        model_list.append(model)
+        perplexity_values.append(np.exp2(-model.log_perplexity(corpus)))
+    return model_list, perplexity_values
+
+
 def plot_graph(documents, start, stop, step, dictionary, corpus):
-    model_list, coherence_values = compute_coherence_values(
-        dictionary, corpus, documents, stop, start, step)
+    coherence_model_list, coherence_values = compute_coherence_values(
+        dictionary,
+        corpus,
+        documents,
+        stop,
+        start,
+        step
+    )
+    perplexity_model_list, perplexity_values = compute_perplexity_values(
+        dictionary,
+        corpus,
+        documents,
+        stop,
+        start,
+        step
+    )
     x = range(start, stop, step)
-    plt.plot(x, coherence_values)
-    plt.xlabel("Number of topics")
-    plt.ylabel("Coherence")
-    plt.legend(("Coherence"), loc='best')
+    fig = plt.figure()
+    ax1 = fig.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(x, coherence_values, label="Coherence", color="b", marker="^")
+    ax2.plot(x, perplexity_values, label="Perplexity", color="g", marker="o")
+    ax1.set_xlabel("number of topics")
+    ax1.set_ylabel("Coherence")
+    ax2.set_ylabel("Perplexity")
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1 + h2, l1 + l2, loc="upper left")
     plt.show()
 
 
 # KH Coderから出力した、文書-単語行列のcsvファイル。
 df = pd.read_csv(
-    './data/file_name.csv',
+    './data/Twitter_gensim.csv',
     sep=',')
 
 # 日本語の取り扱い・語の取捨選択はKH Coderでやる方が楽。
@@ -85,7 +127,11 @@ test_model = models.TfidfModel(corpus)
 # corpusへのモデル適用
 corpus_tfidf = test_model[corpus]
 
-start, stop, step = 2, 30, 1
+# 保存
+with open("twitter_corpus_tfidf.pkl", 'wb') as f:
+    pickle.dump(corpus_tfidf, f)
+
+start, stop, step = 1, 50, 1
 plot_graph(documents, start, stop, step, dictionary, corpus_tfidf)
 
 number_of_topics = 7
